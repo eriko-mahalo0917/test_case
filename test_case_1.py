5cfb1398ce5fd3680233a24f16a0547b26f12e19
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-08-21 07:54:10 +0900

    2つの数字を引数として受け取り、それらを掛け算して結果を戻り値として返す関数

--- test_case_1.py
+++ test_case_1.py
@@ -1,3 +1,13 @@
 def say_hello():
     print('こんにちは！')
 say_hello()
+
+#関数のmultiply_numberに引数a,b
+def multiply_numbers(a,b):
+#result一時的にの保存する変数だからa✕ｂをする　変数の箱！自分で決めてOK
+    result = a * b
+#returnで戻り値
+    return  result
+#productは変数で箱！他の名前にしてもいい　自分で決めてOK
+product = multiply_numbers(3, 5)
+print(product)
