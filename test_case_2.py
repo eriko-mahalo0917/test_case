3ecc68ec498a8f5b0149e6583d5920d30401d00f
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-08-30 20:03:05 +0900

    以下のリストを使って、すべての名前を1つずつ表示する print_names() 関数を作ってください。 names = ["たろう", "はなこ", "じろう"] 条件　1. forを使ってください。

---
+++ test_case_2.py
@@ -0,0 +1,23 @@
+#以下のリストを使って、すべての名前を1つずつ表示する print_names() 関数を作ってください。条件 1. forを使ってください。
+names = ["たろう", "はなこ", "じろう"]
+def print_names(names_list):
+    for name in names:
+        print(name)
+
+print_names(names)
+
+
+names = ["たろう", "はなこ", "じろう"]
+def print_names(name):
+    for name in names:
+        print(name)
+        
+print_names(names)
+
+names = ["john", "paul", "george"]
+def print_names(name):
+    for name in names:
+        print(name)
+        
+        
+print_names(names)
