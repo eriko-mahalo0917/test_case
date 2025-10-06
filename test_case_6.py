4f79eabbac57491392913fffee82a160b1b61a96
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-10-06 11:56:05 +0900

    chrom.pyの練習

--- test_case_6.py
+++ test_case_6.py
@@ -451,4 +451,331 @@ class ChromeManager:
     
     #専門家②ブラウザの起動係
     def start_chrome(self):
+        logging.info("ブラウザを起動準備を開始します")
+        #専門家１を呼び出して、手渡してくれた設定を受け取る
+        options = self.get_chrome_option()
+        service = Service()
+        
+        logging.info("これからブラウザを起動します")
+        chrome = webdriver.Chrome(service=service,options=options)
+        logging.info("ブラウザを起動しました")
+        #起動したブラウザのreturnで受け取っちゃう
+        return chrome
+    
+    def open_site(self,url):
+        #専門家２を受け取る
+        chrome_browser = self.start_chrome()
+        
+        logging.info(f"サイトを開きます：{url}")
+        #受け取ったリモコンでサイトに移動するように命令する
+        chrome_browser.get(url)
+        logging.info("サイトを開きました")
+        
+        input("確認後はエンターを押してサイトを閉じてください")
+        
+#実際にこれを動かしてみるぞ！
+# 1. 設計図からロボット（インスタンス）を作る
+chrome_manager = ChromeManager()
+# 2. 開きたいサイトのURLを用意する
+amazon_url = "https://www.amazon.co.jp/"
+# 3. ロボットに命令を出す
+chrome_manager.open_site(url=amazon_url)
+
+
+#ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+#インポートしてくる
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを作る
+class ChromeManager:
+    #コンストラクタで初期値を設置　ここではログを毎回出すからそこを設定する
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO ,format='%(asctime)s - %(levelname)s - %(message)s')
+        print("ChromeManagerロボットの準備ができました！")
+        
+    #①ブラウザのを設定する
+    def get_chrome_options(self):
+        logging.info("ブラウザのオプションを設定します")
+        #Optionクラスから設定を呼び出す
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=1000,800")
+        chrome_options.add_argument("--window-position=100,100")
+        return chrome_options
+    
+    
+    #②ブラウザを起動する
+    def start_chrome(self):
+        logging.info("ブラウザを起動準備を開始します")
+        #自分自身の機能を使うからselfがいるよ！
+        options = self.get_chrome_options()
+        service = Service()
+        
+        logging.info("これからブラウザを起動します")
+        #準備した設定のoptionsとserviceを渡してブラウザを起動
+        chrome = webdriver.Chrome(service=service , options= options)
+        logging.info("ブラウザを起動しました")
+        return chrome
+        
+    def open_site(self,url):
+        #専門家②に命令して、完成させてものをここで受け取ってサイトを開く
+        chrome_browser = self.start_chrome()
+        
+        logging.info(f"サイトを開きます：{url}")
+        #受け取ったものでサイトに移動するように命令する
+        chrome_browser.get(url)
+        logging.info("サイトを開きました")
+        
+        input("確認後はエンターを押してサイトを閉じてください")
+        
+#実際に動かしてみよう！インスタンスを作成する
+chrome_manager = ChromeManager()
+#開きたいサイトをしてい
+amazon_url = "https://www.amazon.co.jp/"
+#ロボットに命令をする
+chrome_manager.open_site(url=amazon_url)
+
+#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを作成する
+class ChromeManager:
+    #初期設定をする　共通のログの出力形式を指定
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+        print("ChromeManagerロボット準備ができました")
+        
+    #【専門家1】オプション設定係のメソッドを作る
+    def get_chrome_options(self):
+        
+        logging.info("ブラウザのオプションを設定します")
+        #Optionsのクラスからクラスを呼び出す
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=1000,1000")
+        chrome_options.add_argument("--window-position=100,100")
+        return chrome_options
+    
+    # 【専門家2】ブラウザ起動係のメソッドを作る
+    def start_chrome(self):
+        logging.info("ブラウザを起動準備を開始します")
+        options = self.get_chrome_options()
+        service = Service()
+        
+        logging.info("ブラウザを起動します")
+        chrome = webdriver.Chrome(service=service,options=options)
+        logging.info("ブラウザを起動しました")
+        return chrome
+    
+    #【司令塔】全体の流れを管理するメソッドを作る
+    def open_site(self,url):
+        #専門家２を呼び出して代入する
+        chrome_browser = self.start_chrome()
+        
+        logging.info(f"サイトを開きます:{url}")
+        chrome_browser.get(url)
+        logging.info("サイトを開きました")
+        
+        input("確認が終わったらエンターを押して閉じてください")
+        
+    
+        
+#実際に動かす
+chrome_manager = ChromeManager()
+youtube_url = "https://www.youtube.com/"
+chrome_manager.open_site(url = youtube_url)
+
+
+#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝もう１回ーーー
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを設定する
+class ChromeManager:
+    #初期設定　ログをすべて共通へ
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO ,format='%(asctime)s - %(levelname)s - %(message)s')
+        print("ChromeManagerの準備ができました！")
+        
+    #専門家①オプション係を設置
+    def get_chrome_options(self):
+        logging.info("ブラウザのオプションを設定します")
+        #オプションのためのインスタンスを準備
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=900,900")
+        chrome_options.add_argument("--window-position=300,50")
+        return chrome_options
+    
+    #専門家②ブラウザの起動係のメソッド
+    def start_chrome(self):
+        logging.info("ブラウザを起動を開始します")
+        
+        options = self.get_chrome_options()
+        #serviceの準備を忘れない
+        service = Service()
+        #ここのChromeにセッティングしたないようで後からgetで開く
+        chrome = webdriver.Chrome(service=service ,options=options)
+        logging.info("ブラウザを起動しました")
+        return chrome
+    
+    #司令塔で全体を管理
+    def open_site(self,url):
+        
+        #専門家②を読んできて、代入する
+        chrome_browser = self.start_chrome()
+        logging.info(f"サイトを開きます{url}")
+        chrome_browser.get(url)
+        logging.info("サイトを開きました")
+        
+        input("確認が終わったらエンターを押して閉じてください")
+        
+        
+#実際に動かす
+chrome_manager = ChromeManager()
+asahi_url = "https://www.asahi.com/"
+chrome_manager.open_site(url = asahi_url)
+
+#try...exceptで、もしもの時に備える＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+#インポートしてくる
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを作る
+class ChromeManager:
+    #初期設定　ログはすべて共通だからここで準備
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO ,format='%(asctime)s - %(levelname)s - %(message)s')
+        print("ChromeManagerの準備ができました！")
+        
+    #①ブラウザの設定はどんな感じにするのか指定する
+    def get_chrome_options(self):
+        logging.info("ブラウザのオプションを設定します")
+        #インストラクタを設定
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=900,900")
+        chrome_options.add_argument("--window-position=300,50")
+        #設定した内容をreturnで渡してもらう
+        return chrome_options
+    
+    #②ブラウザの起動のメソッド
+    def start_chrome(self):
+        logging.info("ブラウザの起動準備を開始します")
+        options = self.get_chrome_options()
+        service = Service()
+        
+        #＝＝＝＝＝＝▼▼ここからが安全装置▼▼＝＝＝＝＝＝＝
+        try:
+            #挑戦してみて
+            logging.info("これからブラウザを起動します")
+            chrome = webdriver.Chrome(service=service ,options=options)
+            logging.info("ブラウザを起動しました")
+            return chrome
+        
+        except Exception as e:
+            #もしも失敗したときはこれをやる　エラーレベルのログ記録
+            logging.error("ブラウザを起動できませんでした")
+            logging.error(f"error内容{e}")
+            #エラーが発生したことを、プログラム全体に知らせて処理を停止しなさい
+            print("ブラウザの起動に失敗したため処理を終了します")
+            raise
+            
+            #＝＝＝＝＝▲▲安全装置はここまで▲▲＝＝＝＝＝＝＝
+            
+    #司令塔の全体管理
+    def open_site(self,url):
+        #②を呼び出す
+        chrome_browser = self.start_chrome()
+        
+        logging.info(f"サイトを開きます：{url}")
+        chrome_browser.get(url)
+        logging.info("サイトを開きました") 
+        
+        input("確認が終わったらエンターで閉じてね") 
+        
+#実際に動かす
+chrome_manager = ChromeManager()
+asahi_url = "https://www.asahi.com/"
+chrome_manager.open_site(url=asahi_url)
+
+#=========================================================
+#インポートしてくる
+import logging
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを作る
+class ChromeManager:
+    #コンストラクタで初期設定　ログは共通だから
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO ,format='%(asctime)s - %(levelname)s - %(message)s')
+        print("ChromeManagerの準備ができました！")
+        
+    #ブラウザのオプション設定をするメソッドを作成
+    def get_chrome_options(self):
+        logging.info("ブラウザのオプションを設定します")
+        
+        #インストラクタを呼び出す
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=1000,1000")
+        chrome_options.add_argument("--window-position=300,100")
+        #設定した内容を渡してもらう
+        return chrome_options
+    
+    #ブラウザの起動メソッド
+    def start_chrome(self):
+        logging.info("ブラウザの起動準備をします")
+        
+        options = self.get_chrome_options()
+        service = Service()
+        
+        #安全装置を設定
+        try:
+            logging.info("これからブラウザを起動します")
+            chrome = webdriver.Chrome(service=service ,options=options)
+            logging.info("ブラウザを起動しました")
+            return chrome
+        
+        except Exception as e:
+            #もしも失敗したときにやる処理
+            logging.error("ブラウザを起動できませんでした")
+            logging.error(f"error内容：{e}")
+            #errorが発生したから処理をストップ
+            raise
+        
+    #最後のサイトを開く部分
+    def open_site(self,url):
+        chrome_browser = self.start_chrome()
+        
+        logging.info(f"サイトを開きます：{url}")
+        chrome_browser.get(url)
+        logging.info("サイトを開きました")
+        
+        input("確認が終わったらエンターで閉じてね")
+        
+#実際に動かす
+chrome_manager = ChromeManager()
+asahi_url = "https://www.asahi.com/"
+chrome_manager.open_site(url=asahi_url)
+
+#＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄＄
+#インポートしてくる
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+#クラスを作成
+class ChromeManager:
+    #コンストラクタで初期設定
+    def __init__(self):
+        #別ファイルのloggerを呼ぶ
+        self.getLogger = Logger()
         
